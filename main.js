4d7237ac49ce44a38d0f9b0e4862291d8be4789a
Nik Donogan <nik.donogan99@gmail.com>
2026-01-31 13:12:54 +0100

    commit 5.5

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-<link
+/* <link
   rel="preload"
   href="style.css"
   as="style"
   onload="var useThis=this; useThis.onload=null; setTimeout(function(){ useThis.rel='stylesheet'; },3000);"
-></link>;
+></link>; */
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "nextscr" ? 1 : -1;
+    const screenshots = button
+      .closest("[data-carousel]")
+      .querySelector("[data-screenshots]");
+
+    const activeScreenshot = screenshots.querySelector("[data-active]");
+    let newIndex = [...screenshots.children].indexOf(activeScreenshot) + offset;
+    if (newIndex < 0) newIndex = screenshots.children.length - 1;
+    if (newIndex >= screenshots.children.length) newIndex = 0;
+
+    screenshots.children[newIndex].dataset.active = true;
+    delete activeScreenshot.dataset.active;
+  });
+});
