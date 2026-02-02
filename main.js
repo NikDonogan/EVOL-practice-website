26944886277f74de4d0aed454dd267dd015a681c
Nik Donogan <nik.donogan99@gmail.com>
2026-02-02 04:39:50 +0100

    commit 5.7

--- main.js
+++ main.js
@@ -22,3 +22,21 @@ buttons.forEach((button) => {
     delete activeScreenshot.dataset.active;
   });
 });
+
+const gameButtons = document.querySelectorAll("[data-game-button]");
+gameButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.gameButton === "nextgame" ? 1 : -1;
+    const protags = button
+      .closest("[data-protags]")
+      .querySelector("[data-protagsplash]");
+
+    const activeProtag = protags.querySelector("[data-active]");
+    let newIndex = [...protags.children].indexOf(activeProtag) + offset;
+    if (newIndex < 0) newIndex = protags.children.length - 1;
+    if (newIndex >= protags.children.length) newIndex = 0;
+
+    protags.children[newIndex].dataset.active = true;
+    delete activeProtag.dataset.active;
+  });
+});
